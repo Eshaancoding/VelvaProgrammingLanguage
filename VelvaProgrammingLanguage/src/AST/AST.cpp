c83178a10af8236fd7621c1c979d4fc48b5963d3
danieldsilva <dsfamily9@gmail.com>
2022-11-18 18:32:17 -0500

    who added AST.hpp to gitignore

---
+++ VelvaProgrammingLanguage/src/AST/AST.cpp
@@ -0,0 +1,27 @@
+#include "llvm/IR/Constants.h"
+
+#include "AST.hpp"
+
+Value *IntExpr::codegen() {
+    
+}
+
+Value *CallFuncExpr::codegen() {
+
+}
+
+Value *DeclareFunctionExpr::codegen() {
+
+}
+
+Value *StringExpr::codegen() {
+
+}
+
+Value *VarUseExpr::codegen() {
+
+}
+
+Value *VarDeclareExpr::codegen() {
+
+}
