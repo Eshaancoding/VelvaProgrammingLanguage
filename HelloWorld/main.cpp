4ab76ef4c5753206b8bc37972c7a810da1eb4e5f
Eshaancoding <eshaanbarkataki@gmail.com>
2022-11-18 20:27:22 -0500

    CmakeLists changes

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+    // This is just used for testing out the autocomplete
+
+    // Also used for meme purposes. Wouldn't it be funny to just include a c++ hello world inside a compiler for a programming language? lol
+    cout << "Hello World" << endl;
+}
